5122dd1e0e78395801a31ce094763e2ae0de071f
Gab <ghebert10@gmail.com>
2015-02-28 11:23:24 -0500

    enemy modif, gamemaster modif

---
+++ Assets/EndZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Enemy")){
+			Destroy(other.gameObject);
+			GameMaster.instance.safeEnemy();
+		}
+	}
+}
