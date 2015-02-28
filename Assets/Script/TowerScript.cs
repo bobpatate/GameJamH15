376eb6c407d4ac9606a958c038007cbc542755dd
zeen1x <fran6.desbiens@gmail.com>
2015-02-28 08:36:07 -0500

    Update TowerScript

--- Assets/Script/TowerScript.cs
+++ Assets/Script/TowerScript.cs
@@ -3,18 +3,42 @@ using System.Collections;
 
 public class TowerScript : MonoBehaviour {
 
+    private Transform target = null;
+    private float timeToAttack = 0.0f;
+    private float nextAttackTime = 0.0f;
+
+    public static bool isBeingHitted;
+    public float reloadTime = 60.0f;
+    public float damage = 10.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        if (target && (Time.time <= nextAttackTime))
+        {
+            target.GetComponent<EnemyBehaviour>().addFear(damage);
+            timeToAttack = 0.0f;
+            nextAttackTime = Time.time + reloadTime;
+        } 		
 	}
 
     void OnTriggerEnter( Collider other)
     {
-        
+        if (other.gameObject.tag == "Enemy")
+        {
+            target = other.gameObject.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.transform == target)
+        {
+            target = null;
+        }
     }
 }
