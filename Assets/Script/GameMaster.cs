a94381dec11c7bbb84469be9a2bdcd78d0b39278
Gab <ghebert10@gmail.com>
2015-02-28 01:39:39 -0500

    initial gamemaster + gui restore

---
+++ Assets/Script/GameMaster.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*<summary>
+ * Game's central entity. Is a singleton.
+</summary>*/
+
+public class GameMaster : MonoBehaviour {
+	private static GameMaster _instance;
+	
+	public static GameMaster instance{
+		get{
+			if(_instance == null){
+				_instance = GameObject.FindObjectOfType<GameMaster>();
+				DontDestroyOnLoad(_instance.gameObject);
+			}
+			return _instance;
+		}
+	}
+
+	public int currentLevel = 0;
+
+	[SerializeField] private int enemiesToSpawn = 10;
+	private int enemiesLeftToSpawn;
+
+	void Awake(){
+		if(_instance == null){
+			_instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else{
+			if(this != _instance)
+				Destroy(this.gameObject);
+		}
+	}
+
+	void Start(){
+		StartLevel (currentLevel);
+	}
+
+	void Update(){
+
+	}
+
+	void StartLevel(int currentLevel){
+		//enemiesToSpawn += enemiesToSpawn * 1.2;
+		enemiesLeftToSpawn = enemiesToSpawn;
+	}
+
+	void SpawnEnemy(){
+
+	}
+
+	void DeadEnemy(){
+		enemiesLeftToSpawn --;
+		//
+	}
+}
