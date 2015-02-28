5ce35c317e9a8bd4045260b3db23efcaf53c510b
zeen1x <fran6.desbiens@gmail.com>
2015-02-28 09:55:00 -0500

    ADD - FearBar above enemy

--- Assets/Script/EnemyScripts/EnemyBehaviour.cs
+++ Assets/Script/EnemyScripts/EnemyBehaviour.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class EnemyBehaviour : MonoBehaviour
 {
     public Vector3 targetPoint = new Vector3(20, 20, 20); //Determine it through an empty GameObject corresponding to the exit of the map
-    float fear = 0;
-    float maxFear = 100;
+    float fear = 0.0f;
+    float maxFear = 100.0f;
 
     private NavMeshAgent agent;
 
@@ -25,6 +25,16 @@ public class EnemyBehaviour : MonoBehaviour
     {
         fear += fear_to_add;
         checkHorrified();
+    }
+
+    public float getFear()
+    {
+        return fear;
+    }
+
+    public float getMaxFear()
+    {
+        return maxFear;
     }
 
     private void checkHorrified()
---
+++ Assets/Script/EnemyScripts/FearBar.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FearBar : MonoBehaviour
+{
+    private Texture2D background;
+    private Texture2D foreground;
+
+    private float fear = 0.0f;
+    private float maxFear = 0.0f;
+
+    void Start()
+    {
+        //Initialisation
+        fear = GetComponent<EnemyBehaviour>().getFear();
+        maxFear = GetComponent<EnemyBehaviour>().getMaxFear();
+
+        background = new Texture2D(1, 1, TextureFormat.RGB24, false);
+        foreground = new Texture2D(1, 1, TextureFormat.RGB24, false);
+
+        background.SetPixel(0, 0, Color.black);
+        foreground.SetPixel(0, 0, Color.yellow);
+
+        background.Apply();
+        foreground.Apply();
+    }
+
+    void Update()
+    {
+        fear = GetComponent<EnemyBehaviour>().getFear();
+        fear += Input.GetAxisRaw("Horizontal");
+        if (fear < 0.0f) fear = 0.0f;
+        if (fear > maxFear) fear = maxFear;
+    }
+
+    void OnGUI()
+    {
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);// gets screen position.
+        screenPosition.y = Screen.height - (screenPosition.y + 1);// inverts y
+
+        Rect box = new Rect(screenPosition.x - 50, screenPosition.y - 20, 100, 15);
+
+        GUI.DrawTexture(new Rect(box.x, box.y, box.width, box.height), background, ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(box.x, box.y, box.width * fear / maxFear, box.height), foreground, ScaleMode.StretchToFill);
+        GUI.Box(box, "");
+    }
+
+}
