e2765e0c58d5adba685a20091dbce73ebef934ae
Yohann Jardin <yohann.jardin@telecomnancy.net>
2015-02-27 23:06:28 -0500

    EnemyBehaviour created. Fear and pathfinding implemented. Lack sprite and animation. For the pathfinding to work properly, map components will need to tell whether they are walkable or not.

---
+++ Assets/Script/EnemyBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Vector3 targetPoint = new Vector3(20, 20, 20); //Determine it through an empty GameObject corresponding to the exit of the map
+    float fear = 0;
+    float maxFear = 100;
+
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = new NavMeshAgent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(targetPoint);
+    }
+
+    public void addFear(float fear_to_add)
+    {
+        fear += fear_to_add;
+        checkHorrified();
+    }
+
+    private void checkHorrified()
+    {
+        if (fear >= maxFear)
+        {
+            launchHorrifiedAnimation();
+        }
+    }
+
+    private void launchHorrifiedAnimation()
+    {
+        Destroy(gameObject); //TODO
+    }
+
+
+}
