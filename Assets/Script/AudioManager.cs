7bad2fdddb6d697b4fe04e747f034229c33e042d
Gab <ghebert10@gmail.com>
2015-03-01 09:30:53 -0500

    some sound

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+	private static AudioManager _instance;
+	
+	public static AudioManager instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = GameObject.FindObjectOfType<AudioManager>();
+			}
+			return _instance;
+		}
+	}
+
+	public AudioClip ballHit;
+	public AudioClip buildReload;
+	public AudioClip menuSelect;
+	public AudioClip menuShift;
+	public AudioClip Success;
+	public AudioClip Scream;
+	
+	public void playBallHitSound(){
+		audio.PlayOneShot(ballHit);
+	}
+	public void playBuildReloadSound(){
+		audio.PlayOneShot(buildReload);
+	}
+	public void playMenuSelectSound(){
+		audio.PlayOneShot(menuSelect);
+	}
+	public void playMenuShiftSound(){
+		audio.PlayOneShot(menuShift);
+	}
+	public void playSuccessSound(){
+		audio.PlayOneShot(Success);
+	}
+	public void playScreamSound(){
+		audio.PlayOneShot(Scream);
+	}
+}
