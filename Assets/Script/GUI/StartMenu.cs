5e4437cac4fe37a8c62d949fb4eb81272ae68728
Yohann Jardin <yohann.jardin@telecomnancy.net>
2015-03-01 04:26:02 -0500

    New scene: menu

---
+++ Assets/Script/GUI/StartMenu.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+
+    private int currentPos;
+    private bool showControl = false;
+    private bool showNewGameGUI = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPos = 0;
+        changeSprite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changePos(int side)
+    {
+        currentPos = (currentPos + side) % 3;
+        currentPos = currentPos == -1 ? 2 : currentPos;
+        changeSprite();
+    }
+
+    public int getCurrentPos()
+    {
+        return currentPos;
+    }
+
+    public void changeSprite()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name.Equals("NewGame"))
+            {
+                if (currentPos == 0)
+                {
+                    child.GetComponent<Image>().color = Color.red;
+                }
+                else
+                {
+                    child.GetComponent<Image>().color = Color.white;
+                }
+
+            }
+            else if (child.name.Equals("Controls"))
+            {
+                if (currentPos == 1)
+                {
+                    child.GetComponent<Image>().color = Color.red;
+                }
+                else
+                {
+                    child.GetComponent<Image>().color = Color.white;
+                }
+            }
+            else
+            {
+                if (currentPos == 2)
+                {
+                    child.GetComponent<Image>().color = Color.red;
+                }
+                else
+                {
+                    child.GetComponent<Image>().color = Color.white;
+                }
+            }
+        }
+    }
+
+    public void showControls()
+    {
+        showControl = true;
+    }
+
+    public void hideControls()
+    {
+        showControl = false;
+    }
+
+    public void showNewGame()
+    {
+        showNewGameGUI = true;
+    }
+
+    public void hideNewGame()
+    {
+        showNewGameGUI = false;
+    }
+
+    void OnGUI()
+    {
+        if (showControl)
+        {
+
+        }
+        if (showNewGameGUI)
+        {
+
+        }
+    }
+}
---
+++ Assets/Script/GUI/StartMenuController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuController : MonoBehaviour
+{
+
+    bool VDPadInUse = false;
+    StartMenu sm;
+
+    // Use this for initialization
+    void Start()
+    {
+        sm = transform.GetComponent<StartMenu>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            switch (transform.GetComponent<StartMenu>().getCurrentPos())
+            {
+                case 0:
+                    //New game
+                    sm.showNewGame();
+                    break;
+                case 1:
+                    //Show gui controls
+                    sm.showControls();
+                    break;
+                case 2:
+                    //Quit
+                    Application.Quit();
+                    break;
+            }
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            sm.hideNewGame();
+            sm.hideControls();
+        }
+
+        if (Input.GetAxis("VDPad") < 0)
+        {
+            if (!VDPadInUse)
+            {
+                sm.changePos(1);
+                VDPadInUse = true;
+            }
+        }
+        if (Input.GetAxis("VDPad") > 0)
+        {
+            if (!VDPadInUse)
+            {
+                sm.changePos(-1);
+                VDPadInUse = true;
+            }
+        }
+        if (Input.GetAxis("VDPad") == 0)
+        {
+            VDPadInUse = false;
+        }
+    }
+}
